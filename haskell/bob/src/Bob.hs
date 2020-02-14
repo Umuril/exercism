b63dfb37245303980c7c09c957b2db7d01a57383
Umuril Lyerood <umuril.lyerood@gmail.com>
2020-02-14 12:01:09 +0100

    Add Bob

---
+++ haskell/bob/src/Bob.hs
@@ -0,0 +1,17 @@
+module Bob (responseFor) where
+import qualified Data.Char as C
+
+isYelling :: String -> Bool
+isYelling = and . sequence [not . null, all C.isUpper] . filter C.isAlpha
+
+isQuestion :: String -> Bool
+isQuestion [] = False
+isQuestion (x:xs) | C.isSpace x = isQuestion xs | otherwise = x == '?'
+
+responseFor :: String -> String
+responseFor xs  | all C.isSpace xs = "Fine. Be that way!"
+                | isQuestion (reverse xs)  = case () of 
+                 _ | isYelling xs -> "Calm down, I know what I'm doing!"
+                    | otherwise -> "Sure."
+                | isYelling xs = "Whoa, chill out!"
+                | otherwise = "Whatever."
