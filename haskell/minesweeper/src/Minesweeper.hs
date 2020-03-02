9be00111b3c6e6af2c4cc23eb7fcecc9c5012214
Umuril Lyerood <umuril.lyerood@gmail.com>
2020-03-02 14:47:12 +0100

    Modified Minesweeper

--- haskell/minesweeper/src/Minesweeper.hs
+++ haskell/minesweeper/src/Minesweeper.hs
@@ -2,36 +2,20 @@ module Minesweeper (annotate) where
 import qualified Data.Char as C
 import qualified Data.Matrix as M
 import qualified Data.Vector as V
-import Debug.Trace
 
-traceThis :: (Show a) => a -> a
-traceThis x = trace ("test: " ++ show x) x
+createTuple :: (Int, Int) -> Char -> (Int, Int, Char)
+createTuple (r,c) value = (r, c, value)
 
-if' :: Bool -> a -> a -> a
-if' True  x _ = x
-if' False _ y = y
+isNear :: (Int, Int) -> (Int, Int) -> Bool
+isNear (x1, y1) (x2, y2) = abs (x1 - x2) <= 1 && abs (y1 - y2) <= 1
 
-opt' :: a -> a -> a
-opt' value def | null value = def | otherwise = value
-
-toChar :: Int -> Char
-toChar 0 = ' '
-toChar x = C.intToDigit x
-
-check :: Int -> (Int -> Int -> Bool) -> Int -> Int
-check a op b | op a b = traceThis a | otherwise = traceThis b
-
-countAround :: M.Matrix Char -> Char
-countAround m   | m M.! (2,2) == '*' = '*' 
-                | otherwise = toChar( V.length( V.filter (\x -> x == '*') (M.getMatrixAsVector m)))
-
-swipe :: M.Matrix Char -> M.Matrix Char
-swipe m = M.mapPos (\(r,c) _ -> countAround (M.submatrix (check (r-1) (>) 1) (check (c-1) (>) 1) (check (r+1) (<) (M.nrows m)) (check (c+1) (<) (M.ncols m)) m)) m
+setValue :: [(Int, Int)] -> (Int, Int) -> Char -> Char
+setValue points (r,c) value
+    | value == '*' = '*'
+    | near == 0 = ' '
+    | otherwise = C.intToDigit $ near
+    where near = length $ filter (isNear (r, c)) points
 
 annotate :: [String] -> [String]
-annotate board = do
-    let x = []
-    
-
--- Iterare su tutti i caratteri e prendersi una lista delle posizioni di ogni *
--- Per ogni punto nella matrice contare tutti gli asterischi che sono a "distanza di 1"
+annotate [] = []
+annotate board = M.toLists $ M.mapPos (setValue [(x,y) | (x, y, value) <- V.toList $ M.getMatrixAsVector $ M.mapPos createTuple $ M.fromLists board, value == '*']) $ M.fromLists board
