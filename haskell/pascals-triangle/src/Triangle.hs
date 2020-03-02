4997baf56fa83fa2b2630d139bae975520606967
Umuril Lyerood <umuril.lyerood@gmail.com>
2020-03-02 15:54:56 +0100

    Add Pascals Triangle

---
+++ haskell/pascals-triangle/src/Triangle.hs
@@ -0,0 +1,8 @@
+module Triangle (rows) where
+import qualified Data.List as L
+
+nextRow :: [Integer] -> Maybe ([Integer], [Integer])
+nextRow row = Just (row, zipWith (+) (row ++ [0]) ([0] ++ row))
+
+rows :: Int -> [[Integer]]
+rows x = take x $ L.unfoldr nextRow [1]
