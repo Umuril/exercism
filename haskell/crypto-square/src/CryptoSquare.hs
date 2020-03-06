84a2b55760e7567323166a19a2a0a259aab26871
Umuril Lyerood <umuril.lyerood@gmail.com>
2020-03-06 10:29:22 +0100

    Modified Crypto Square

--- haskell/crypto-square/src/CryptoSquare.hs
+++ haskell/crypto-square/src/CryptoSquare.hs
@@ -1,18 +1,19 @@
-{-# LANGUAGE OverloadedStrings #-}
-
 module CryptoSquare (encode) where
 import qualified Data.Text as T
 import qualified Data.Char as C
 
-addTrailing :: Int -> [String] -> [String]
-addTrailing n = map $ T.justifyLeft n ' '
+addTrailing :: Int -> [T.Text] -> [T.Text]
+addTrailing = map . (`T.justifyLeft` ' ')
 
-splitInchunks :: String -> [String]
-splitInchunks xs = addTrailing r (T.chunksOf r xs)
+splitInchunks :: T.Text -> [T.Text]
+splitInchunks xs = addTrailing r $ T.chunksOf r xs
     where r = ceiling $ sqrt $ (fromIntegral :: Int -> Double) $ T.length xs 
 
 removePunctuation :: String -> String
-removePunctuation = T.filter $ not . or . sequence [C.isSpace, C.isPunctuation]
+removePunctuation = filter $ not . or . sequence [C.isSpace, C.isPunctuation]
+
+toLowerString :: String -> String
+toLowerString = map C.toLower
 
 encode :: String -> String
-encode = T.unwords . T.transpose . splitInchunks . T.toLower . removePunctuation
+encode = T.unpack . T.unwords . T.transpose . splitInchunks . T.pack . toLowerString . removePunctuation
