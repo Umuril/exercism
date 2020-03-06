8a519deff8c0b4ae227ae974c9b7b80894ed674a
Umuril Lyerood <umuril.lyerood@gmail.com>
2020-03-06 09:40:03 +0100

    Modified Pascals Triangle

--- haskell/pascals-triangle/src/Triangle.hs
+++ haskell/pascals-triangle/src/Triangle.hs
@@ -2,7 +2,7 @@ module Triangle (rows) where
 import qualified Data.List as L
 
 nextRow :: [Integer] -> Maybe ([Integer], [Integer])
-nextRow row = Just (row, zipWith (+) (row ++ [0]) ([0] ++ row))
+nextRow row = Just (row, zipWith (+) (row ++ [0]) ([0] : row))
 
 rows :: Int -> [[Integer]]
 rows x = take x $ L.unfoldr nextRow [1]
