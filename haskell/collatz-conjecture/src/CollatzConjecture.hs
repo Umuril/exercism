e3643a976ef0b3ed030ffc33fcf179859dc31725
Umuril Lyerood <umuril.lyerood@gmail.com>
2020-11-13 09:11:15 +0100

    Modified CollatzConjecture

--- haskell/collatz-conjecture/src/CollatzConjecture.hs
+++ haskell/collatz-conjecture/src/CollatzConjecture.hs
@@ -1,12 +1,14 @@
 module CollatzConjecture (collatz) where
 
-import Data.List
+import Data.Maybe
 
-nextNumber :: (Integer -> Maybe (Integer, Integer))
-nextNumber n    | n < 2 = Nothing
-                | mod n 2 == 0 = Just (n, quot n 2)
-                | otherwise = Just (n, 3 * n + 1)
+-- TO REFACTOR
+
+nextN :: Integer -> Integer
+nextN n    | mod n 2 == 0 = quot n 2
+           | otherwise = 3 * n + 1
 
 collatz :: Integer -> Maybe Integer
-collatz x   | x < 1 = Nothing 
-            | otherwise = Just $ toInteger $ length $ takeWhile (>1) $ unfoldr nextNumber x
+collatz x   | x == 1 = Just 0
+            | x > 1  = Just $ 1 + fromJust (collatz $ nextN x)
+            | otherwise = Nothing
