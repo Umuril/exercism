02ea7d7dc0812b11c7fb474059175990ba8e25f9
Umuril Lyerood <umuril.lyerood@gmail.com>
2020-11-13 09:12:55 +0100

    Modified Prime

--- haskell/nth-prime/src/Prime.hs
+++ haskell/nth-prime/src/Prime.hs
@@ -1,7 +1,11 @@
 module Prime (nth) where
 
-is_prime :: Integer -> Bool
-is_prime n | n == 1 = False | n == 2 = True | (length [x | x <- [2 .. n-1], mod n x == 0]) > 0 = False | otherwise = True
+isPrime :: Integer -> Bool
+isPrime n   | n == 1 = False 
+            | n == 2 = True 
+            | not $ null [x | x <- [2 .. n-1], mod n x == 0] = False 
+            | otherwise = True
 
 nth :: Int -> Maybe Integer
-nth n | n == 0 = Nothing | otherwise = Just ((filter is_prime [1..]) !! (n-1))
+nth 0 = Nothing
+nth n = Just (filter isPrime [1..] !! (n-1))
