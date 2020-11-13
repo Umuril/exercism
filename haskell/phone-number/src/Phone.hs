7ad26a7a21920d5ed1872523b632ef758762aac3
Umuril Lyerood <umuril.lyerood@gmail.com>
2020-11-13 09:14:50 +0100

    Modified Phone

--- haskell/phone-number/src/Phone.hs
+++ haskell/phone-number/src/Phone.hs
@@ -3,8 +3,11 @@ module Phone (number) where
 import qualified Data.Char as C
 import Text.Regex.PCRE
 
+-- TO BE REVIEWED
+
 if' :: (a -> Bool) -> a -> Maybe a
 if' test value | test value = Just value | otherwise = Nothing
 
 number :: String -> Maybe String
-number xs = if' (not . null) ((filter C.isDigit xs) =~ "(?(?<=^1)([2-9][0-9]{2}){2}[0-9]{4}$|^([2-9][0-9]{2}){2}[0-9]{4}$)$" :: String) 
+-- number xs = if' (not . null) (filter C.isDigit xs =~~ "(?(?<=^1)([2-9][0-9]{2}){2}[0-9]{4}$|^([2-9][0-9]{2}){2}[0-9]{4}$)$" :: String) 
+number xs = (filter C.isDigit xs =~~ "(?(?<=^1)([2-9][0-9]{2}){2}[0-9]{4}$|^([2-9][0-9]{2}){2}[0-9]{4}$)$" :: String) >>= (\x -> Nothing)
