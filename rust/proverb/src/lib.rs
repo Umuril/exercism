ff7ddd2d2edb6456f52654699c051be0c4373527
Umuril <umuril.lyerood@gmail.com>
2020-11-21 09:50:00 +0100

    Modify Rust Proverb

--- rust/proverb/src/lib.rs
+++ rust/proverb/src/lib.rs
@@ -1,9 +1,7 @@
+use std::iter::once;
+
 pub fn build_proverb(list: &[&str]) -> String {
-    if list.is_empty() {
-        return String::new();
-    };
-    let mut result: Vec<String> = list
-        .windows(2)
+    list.windows(2)
         .map(|window| {
             format!(
                 "For want of a {} the {} was lost.",
@@ -11,12 +9,11 @@ pub fn build_proverb(list: &[&str]) -> String {
                 window.get(1).unwrap()
             )
         })
-        .collect();
-
-    result.push(format!(
-        "And all for the want of a {}.",
-        list.get(0).unwrap()
-    ));
-
-    result.join("\n")
+        .chain(once(match list.is_empty() {
+            true => String::new(),
+            false => format!("And all for the want of a {}.", list.get(0).unwrap()),
+        }))
+        .collect::<Vec<String>>()
+        .join("\n")
 }
+
