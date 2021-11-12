243dd08a3555b6cf203e6cefafe412fb55f50a81
Umuril Lyerood <umuril.lyerood@gmail.com>
2021-11-12 21:44:56 +0100

    Add Rust Health Statistics

---
+++ rust/health-statistics/target/debug/deps/health_statistics-bc6ead1c16acd366.d
@@ -0,0 +1,5 @@
+/home/eddy/Projects/exercism/rust/health-statistics/target/debug/deps/health_statistics-bc6ead1c16acd366: tests/health-statistics.rs
+
+/home/eddy/Projects/exercism/rust/health-statistics/target/debug/deps/health_statistics-bc6ead1c16acd366.d: tests/health-statistics.rs
+
+tests/health-statistics.rs:
